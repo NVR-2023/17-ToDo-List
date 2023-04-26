0bdfefdc600afa4ce3ea4b98099682f386aa39a8
NVR-2023 <nunoviolanterodrigues@hotmail.com>
2023-04-26 10:11:09 +0000

    Adjusted formatting - 26/04/2023

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,43 +5,43 @@ import Task from "./task.jsx";
 
 // Home component
 const Home = () => {
-const [listOfTasks, setListOfTasks] = useState([]);
-const [parent] = useAutoAnimate({duration: 250});
+  const [listOfTasks, setListOfTasks] = useState([]);
+  const [parent] = useAutoAnimate({ duration: 250 });
 
-// Task management functions
+  // Task management functions
   const addTask = (taskObject) => {
     taskObject.taskNumber = (listOfTasks.length + 1).toString().padStart(2, '0');;
     setListOfTasks([...listOfTasks, taskObject]);
   };
 
   const deleteTask = (index) => {
-  const temporaryList = [...listOfTasks];
-  temporaryList.splice(index, 1);
-  temporaryList.forEach((item,i) => item.taskNumber = i+1)
-  setListOfTasks(temporaryList);
-};
+    const temporaryList = [...listOfTasks];
+    temporaryList.splice(index, 1);
+    temporaryList.forEach((item, i) => item.taskNumber = i + 1)
+    setListOfTasks(temporaryList);
+  };
 
-const raiseTask = (index) => {
-  if (index <= 0) return false;
-  const temporaryList = [...listOfTasks];
-  const temporaryItem = temporaryList[index];
-  temporaryList[index] = temporaryList[index - 1];
-  temporaryList[index - 1] = temporaryItem;
-  temporaryList.forEach((element, i) => element.taskNumber = i + 1);
-  setListOfTasks(temporaryList);
-  return true;
-};
+  const raiseTask = (index) => {
+    if (index <= 0) return false;
+    const temporaryList = [...listOfTasks];
+    const temporaryItem = temporaryList[index];
+    temporaryList[index] = temporaryList[index - 1];
+    temporaryList[index - 1] = temporaryItem;
+    temporaryList.forEach((element, i) => element.taskNumber = i + 1);
+    setListOfTasks(temporaryList);
+    return true;
+  };
 
-const lowerTask = (index) => {
-  if (index >= listOfTasks.length - 1) return false;
-  const temporaryList = [...listOfTasks];
-  const temporaryItem = temporaryList[index];
-  temporaryList[index] = temporaryList[index + 1];
-  temporaryList[index + 1] = temporaryItem;
-  temporaryList.forEach((element, i) => element.taskNumber = i +1);  
-  setListOfTasks(temporaryList);
-  return true;
-};
+  const lowerTask = (index) => {
+    if (index >= listOfTasks.length - 1) return false;
+    const temporaryList = [...listOfTasks];
+    const temporaryItem = temporaryList[index];
+    temporaryList[index] = temporaryList[index + 1];
+    temporaryList[index + 1] = temporaryItem;
+    temporaryList.forEach((element, i) => element.taskNumber = i + 1);
+    setListOfTasks(temporaryList);
+    return true;
+  };
 
   // JSX Code
   return (
@@ -50,26 +50,26 @@ const lowerTask = (index) => {
         <div className="input-bar g-0 py-3">
           <Input addNewTask={addTask} numberOfTasks={listOfTasks.length} />
         </div>
-          <div className="ul-wrapper" ref={parent}>
-      {listOfTasks&&listOfTasks.map((element, index) => {
-        return (
-          <ul className="task-wrapper">
-            <li key={element.taskId}>
-              <Task
-                taskNumber={index+1}
-                taskDate={element.taskDate}
-                taskLabel={element.taskLabel}
-                taskPriority={element.taskPriority}
-                deleteTask={deleteTask}
-                raiseTask={raiseTask}
-                lowerTask={lowerTask}
-              />
-            </li>
-          </ul>
-        );
-      })}
-    </div>
+        <div className="ul-wrapper" ref={parent}>
+          {listOfTasks && listOfTasks.map((element, index) => {
+            return (
+              <ul className="task-wrapper">
+                <li key={element.taskId}>
+                  <Task
+                    taskNumber={index + 1}
+                    taskDate={element.taskDate}
+                    taskLabel={element.taskLabel}
+                    taskPriority={element.taskPriority}
+                    deleteTask={deleteTask}
+                    raiseTask={raiseTask}
+                    lowerTask={lowerTask}
+                  />
+                </li>
+              </ul>
+            );
+          })}
         </div>
+      </div>
     </div>
   );
 };
